2c5cc186dae217c1c041fe978271a07866fdbf83
Kane026 <frentzkane@gmail.com>
2025-01-15 01:05:27 +0100

    added js scroll animation

---
+++ script.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+        
+    });
+},
+{
+    rootMargin: '0px 0px -20% 0px',
+    threshold: 0.2,
+}
+);
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
